e3a379ab7c44b053ad5795d4b22cfca61c33e0c8
Projects repositories of Professor Salinet (SENAC) <rodrigo.silva@docente.pr.senac.br>
2025-04-04 15:33:35 -0300

    Add files via upload

---
+++ session.js
@@ -0,0 +1,15 @@
+// Gravando uma sessão no javascript
+localStorage.setItem('lastname','Smith'); // grava a sessão 'lastname' com valor 'Smith'
+console.log(localStorage.getItem('lastname'));
+
+// Removendo uma sessão específica em javascript
+Storage.removeItem('lastname'); // apaga a entrada "lastname"
+console.log(localStorage.getItem('lastname'));
+
+// Gravando outra sessão em javascript
+localStorage.setItem('firstname','John'); // grava a sessão 'firstname' com valor 'John'
+console.log(localStorage.getItem('firstname'));
+
+// Limpando todas as sessões em javascript
+Storage.clear(); // apaga tudo o que está no local storage
+console.log(localStorage.getItem('lastname'), localStorage.getItem('firstname'));
