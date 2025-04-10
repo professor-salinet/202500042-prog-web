0026236b573e2f15fecbebbeb8667b1f804b0b71
Projects repositories of Professor Salinet (SENAC) <rodrigo.silva@docente.pr.senac.br>
2025-04-10 17:28:40 -0300

    Add files via upload

--- atualizacao.js
+++ atualizacao.js
@@ -1,38 +1,57 @@
 document.getElementById('frmAtualizacao').addEventListener('submit', async (e) => {
     e.preventDefault();
+    const id = document.getElementById('selId').value;
     const nome = document.getElementById('txtNome').value;
     const login = document.getElementById('txtLogin').value;
+    const senha = document.getElementById('txtSenha').value;
     const notificacao = document.getElementById('notificacao');
 
-    if (nome.trim().length == 0 && login.trim().length == 0) {
-        notificacao.innerHTML = "<b class=\"text-danger\">É necessário digitar um nome ou login para realizar a pesquisa.</b>"
+    if (nome.trim().length == 0) {
+        notificacao.innerHTML = "<b class=\"text-danger\">É necessário digitar um nome para continuar.</b>";
+        txtNome.focus();
         return false;
     }
 
-    const senha = document.getElementById('txtSenha').value;
-    const tipo = 'leitura';
+    if (login.trim().length == 0) {
+        notificacao.innerHTML = "<b class=\"text-danger\">É necessário digitar um login para continuar.</b>";
+        txtLogin.focus();
+        return false;
+    }
+
+    if (senha.trim().length == 0) {
+        notificacao.innerHTML = "<b class=\"text-danger\">É necessário digitar uma senha para continuar.</b>";
+        txtSenha.focus();
+        return false;
+    }
+
+    const txtId = document.getElementById("txtId");
+    const txtNome = document.getElementById("txtNome");
+    const txtLogin = document.getElementById("txtLogin");
+    const txtSenha = document.getElementById("txtSenha");
+    var selId = document.getElementById("selId");
+    selId = selId.options[selId.selectedIndex];
+
+    if (
+        selId.text == txtNome.value && 
+        selId.dataset.login == txtLogin.value && 
+        selId.dataset.senha == txtSenha.value
+    ) {
+        notificacao.innerHTML = "<b class=\"text-danger\">Não há alteração a ser atualizada no banco de dados.</b>";
+        return false;
+    }
+
+    const tipo = 'atualizar';
 
     const response = await fetch('/api/mysql', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ nome, login, senha, tipo })
+        body: JSON.stringify({ nome, login, senha, tipo, id })
     });
 
     const result = await response.json();
     console.log(result.message);
-    // console.log(result.id);
-    // console.log(result.nome);
-    // console.log(result.login);
-    const txtId = document.getElementById('txtId');
-    const txtNome = document.getElementById('txtNome');
-    const txtLogin = document.getElementById('txtLogin');
 
-    txtId.value = result.id;
-    txtNome.value = result.nome;
-    txtLogin.value = result.login;
-    let exibir = (parseInt(result.linhas) > 0) ? " Exibindo o primeiro." : " Nada a exibir.";
-    notificacao.innerText = "Foi/foram encontrado(s) " + result.linhas + " resultado(s)." + exibir;
-    console.log(result.linhas);
+    notificacao.innerText = result.message;
 });
 
 window.addEventListener('load', async (e) => {
@@ -64,6 +83,7 @@ window.addEventListener('load', async (e) => {
         optTmp.dataset.senha = result.rows[i].senha;
         selId.add(optTmp);
     }
+    notificacao.innerText = "Dados carregados com sucesso!";
 });
 
 document.getElementById('selId').addEventListener('change', () => {
